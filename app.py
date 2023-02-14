4fdec0c365b41f617e2d9ddf940ddfb7b8255b7b
kenf1 <103538771+kenf1@users.noreply.github.com>
2023-02-14 17:11:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly_express as px
 import streamlit as sl
 
 #import data
-ittf_rank = pd.read_csv("data/dataset.csv")[:30]
+ittf_rank = pd.read_csv("data/dataset.csv")
 ittf_metadata = pd.read_csv("data/metadata.csv")
 
 #extract year & week
@@ -13,32 +13,63 @@ datasetWeek = ittf_metadata["datasetWeek"][0]
 
 #--- interactive web app ---#
 
+#sidebar title & description
+sl.sidebar.title("Navigation")
+sl.sidebar.markdown("Click on the buttons below to view the different pages of this interactive dashboard.")
+
+#page selector
+options = sl.sidebar.radio("Pages",options=["Home","Countries Represented","Total Points","Rank Changed","Full Dataset"])
+
+#note
+sl.sidebar.markdown("***Note:*** _All figures are interactive. Double-click anywhere inside the figures to reset to default view._")
+
 #site text
-sl.title(f"ITTF Men's Singles Rankings for {datasetYear} {datasetWeek}")
-sl.markdown("By: [KF](https://github.com/kenf1)")
-sl.text("An auto-updating interactive dashboard to visualize the \n countries and total number of points \n for the world's top ranked 30 table tennis players.")
-sl.markdown("Data source: [ITTF Rankings](https://www.ittf.com/rankings/)")
-sl.markdown("***Note:*** _All figures are interactive. Double-click anywhere inside the figures to reset to default view._")
+def homepage():
+    sl.title(f"ITTF Men's Singles Rankings for {datasetYear} {datasetWeek}")
+    sl.markdown("By: [KF](https://github.com/kenf1)")
+    sl.text("An auto-updating interactive dashboard to visualize the \n countries and total number of points \n for the world's top ranked 30 table tennis players.")
+    sl.markdown("Data source: [ITTF Rankings](https://www.ittf.com/rankings/)")
+    sl.image("https://www.theindianwire.com/wp-content/uploads/2018/08/table-tennis.jpeg")
 
 #subset data (only keep top 30 players)
-ittf_rank = ittf_rank[:30]
+ittf_rank_short = ittf_rank[:30]
 
 #figures
-fig1 = px.pie(ittf_rank,"Assoc",title="Countries Represented")
+fig1 = px.pie(ittf_rank_short,"Assoc",title="Countries Represented")
 
-fig2 = px.bar(ittf_rank,x="Points",y="Name",color="Assoc",title="Total Points Count for Each Player")
+fig2 = px.bar(ittf_rank_short,x="Points",y="Name",color="Assoc",title="Total Points Count for Each Player")
 fig2.update_layout(yaxis={'categoryorder':'total ascending'})
 
 #rm NaN rows in Rank Change
-table1 = ittf_rank[~ittf_rank["Rank Change"].isna()]
+table1 = ittf_rank_short[~ittf_rank_short["Rank Change"].isna()]
 
-#display outputs
-sl.markdown("#### The world's top 30 table tennis players are from these countries:")
-sl.plotly_chart(fig1)
+#display pie output
+def countries_rep():
+    sl.markdown("#### The world's top 30 table tennis players are from these countries:")
+    sl.plotly_chart(fig1)
 
-sl.markdown("#### Total points (color coded by country) for each player:")
-sl.markdown("__Format:__ `Name` `SURNAME`")
-sl.plotly_chart(fig2)
+#display bar output
+def total_pt():
+    sl.markdown("#### Total points (color coded by country) for each player:")
+    sl.markdown("__Format:__ `Name` `SURNAME`")
+    sl.plotly_chart(fig2)
 
-sl.markdown("#### All players whose rank has changed (`raised`/`dropped`).")
-sl.dataframe(table1)
+#display table output
+def rankchange():
+    sl.markdown("#### All players whose rank has changed (`raised`/`dropped`).")
+    sl.dataframe(table1)
+
+#display full dataset
+def full_dataset():
+    sl.dataframe(ittf_rank,width=1000,height=750)
+
+if options == "Home":
+    homepage()
+elif options == "Countries Represented":
+    countries_rep()
+elif options == "Total Points":
+    total_pt()
+elif options == "Rank Changed":
+    rankchange()
+elif options == "Full Dataset":
+    full_dataset()
